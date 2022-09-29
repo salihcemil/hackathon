40b665fcf0a54f6fc59e2aa0f14af19b5d88315b
Salih Cemil Cetin <salihcemil@gmail.com>
2022-09-29 11:53:20 +0300

    paymentId tipi değiştiirldi

--- deployments/deployContract.js
+++ deployments/deployContract.js
@@ -1,4 +1,7 @@
 const { ethers } = require("hardhat")
+const denv = require('dotenv').config();
+const fs = require("fs");
+const os = require("os");
 
 const main = async () => {
     const initialName = "hacko";
@@ -10,6 +13,21 @@ const main = async () => {
     const contract = await contractFactory.deploy(initialName);
 
     console.log(`Contract address --> ${contract.address}`);
+
+
+    const ENV_VARS = fs.readFileSync("./.env", "utf8").split(os.EOL);
+
+    // find the env we want based on the key
+    const target = ENV_VARS.indexOf(ENV_VARS.find((line) => {
+        return line.match(new RegExp('CONTRACT_ADDRESS'));
+    }));
+
+    // replace the key/value with the new value
+    ENV_VARS.splice(target, 1, `${'CONTRACT_ADDRESS'}=${contract.address}`);
+
+    // write everything back to the file system
+    fs.writeFileSync("./.env", ENV_VARS.join(os.EOL));
+    //
 }
 
 main()
